1245743b0b0d2577a749258d1b1386ba92f42b05
OliLobster <sgyr1993@163.com>
2022-09-17 19:54:10 +0800

    update url mappings

--- src/rpc/RecommentItem.java
+++ src/rpc/RecommentItem.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RecommentItem
  */
-@WebServlet("/RecommentItem")
+@WebServlet("/recommend")
 public class RecommentItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SearchItem
  */
-@WebServlet("/SearchItem")
+@WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
