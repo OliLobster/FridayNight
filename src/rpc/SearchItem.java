8475ae4123687ecee985bec76366ef1427d43199
OliLobster <sgyr1993@163.com>
2022-09-17 19:56:22 +0800

    doGet is working

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,6 +1,8 @@
 package rpc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,12 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		
+		writer.print("<html><body>");
+		writer.print("<h1>doGet is working</h1>");
+		writer.print("</body></html>");
 	}
 
 	/**
