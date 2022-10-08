529dfc2253731808757bac50896f32543a637542
OliLobster <sgyr1993@163.com>
2022-10-08 17:20:16 +0800

    update url mapping

--- src/rpc/RecommentItem.java
+++ src/rpc/RecommentItem.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RecommentItem
  */
-@WebServlet("/recommend")
+@WebServlet("/recommendation")
 public class RecommentItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
