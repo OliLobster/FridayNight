74e0fa6e1362bd80b3bb5b4d248a45f099247d71
OliLobster <sgyr1993@163.com>
2022-10-11 19:32:01 +0800

    implement both close method and constructor

--- src/db/mysql/MySQLConnection.java
+++ src/db/mysql/MySQLConnection.java
@@ -1,5 +1,11 @@
 package db.mysql;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import java.util.List;
 import java.util.Set;
 
@@ -8,11 +14,28 @@ import entity.Item;
 
 public class MySQLConnection implements DBConnection {
 
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
-
-	}
+	  private Connection conn;
+	   
+	   public MySQLConnection() {
+	  	 try {
+	  		 Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+	  		 conn = DriverManager.getConnection(MySQLDBUtil.URL);
+	  		
+	  	 } catch (Exception e) {
+	  		 e.printStackTrace();
+	  	 }
+	   }
+	   
+	   @Override
+	   public void close() {
+	  	 if (conn != null) {
+	  		 try {
+	  			 conn.close();
+	  		 } catch (Exception e) {
+	  			 e.printStackTrace();
+	  		 }
+	  	 }
+	   }
 
 	@Override
 	public void setFavoriteItems(String userId, List<String> itemIds) {
